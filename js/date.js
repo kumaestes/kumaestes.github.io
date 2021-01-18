bf14d28387aed1b014d26c9112e21a0e16886b99
kumaestes <estesinhawaii@yahoo.com>
2021-01-18 12:57:53 -0700

    update

--- js/date.js
+++ js/date.js
@@ -1,2 +1,4 @@
+//put the current year after copyright//
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+//date of last modification//
 document.getElementById("lastmodified").innerHTML = "Last modified: " + document.lastModified;
